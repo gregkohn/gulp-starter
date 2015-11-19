22a9f601e1b0f19383144997c6b915005af2b442
Greg Kohn <greg.kohn@viget.com>
2015-11-19 12:49:31 -0500

    Adds support for globally and locally accessible JSON files

--- gulpfile.js/tasks/html.js
+++ gulpfile.js/tasks/html.js
@@ -6,6 +6,8 @@ var gulp        = require('gulp')
 var path        = require('path')
 var fs          = require('fs')
 var jade        = require('gulp-jade')
+var data        = require('gulp-data')
+var assign      = require('lodash/object/assign')
 
 var exclude = path.normalize('!**/{' + config.tasks.html.excludeFolders.join(',') + '}/**')
 
@@ -14,11 +16,30 @@ var paths = {
   dest: path.join(config.root.dest, config.tasks.html.dest),
 }
 
+var getData = function(file) {
+  var localPath  = path.resolve(config.root.src, config.tasks.html.src, config.tasks.html.dataFolder, path.basename(file.path)+'.json')
+
+  //The global file specified in at config.tasks.html.globalData is required to exist, even if empty
+  var globalPath = path.resolve(config.root.src, config.tasks.html.src, config.tasks.html.dataFolder, config.tasks.html.globalData)
+  var globalData = JSON.parse(fs.readFileSync(globalPath, 'utf8'))
+
+  //If there's a local file, return it with the global file
+  try {
+    var localData  = JSON.parse(fs.readFileSync(localPath, 'utf8'))
+    return assign(localData, globalData)
+  }
+
+  //Otherwise, just return the global file
+  catch (e) {
+    return globalData
+  }
+}
+
 var htmlTask = function() {
   return gulp.src(paths.src)
+    .pipe(data(getData))
     .pipe(jade({
-      pretty: process.env.NODE_ENV !== 'production',
-      data: JSON.parse( fs.readFileSync('src/html/data/global.json', { encoding: 'utf8' }) )
+      pretty: process.env.NODE_ENV !== 'production'
     }))
     .pipe(gulp.dest(paths.dest))
     .pipe(browserSync.stream())
